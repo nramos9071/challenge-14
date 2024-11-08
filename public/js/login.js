c23833ecb0fc1e8f5bb5810dbc6abb3ec7b43ff6
nramos9071 <nramos9071@gmail.com>
2024-11-07 19:24:10 -0600

    working on routes

---
+++ public/js/login.js
@@ -0,0 +1,20 @@
+const loginHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username').value.trim();
+  const password = document.querySelector('#password').value.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in.');
+    }
+  }
+}
