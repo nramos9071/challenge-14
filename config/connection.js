272a723e8ef051211f12fcdd712c92ec29a86692
nramos9071 <nramos9071@gmail.com>
2024-11-14 19:15:10 -0600

    fixed database port

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'postgres',
-    port: 5432,
+    port: process.env.DB_PORT,
   }
 );
 
